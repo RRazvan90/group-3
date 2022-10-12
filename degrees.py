ef4338e07b2e4f1201591bac75ed268881a4b2f7
Patricia Colomer <patricia.colomer@gmail.com>
2022-10-12 20:52:10 +0100

    added dataframes

--- degrees.py
+++ degrees.py
@@ -1,7 +1,14 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as st
 
 
-data = pd.read_csv("Resources", "degrees-that-pay-back.csv")
-data.head()
+df_degrees = pd.read_csv(os.path.join("Resources", "degrees-that-pay-back.csv"))
+print(df_degrees.head())
+
+df_college = pd.read_csv(os.path.join("Resources", "salaries-by-college-type.csv"))
+
+df_region = pd.read_csv(os.path.join("Resources", "salaries-by-region.csv"))
+
+print(df_college.head())
