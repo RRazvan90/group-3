7cf037492a5d78be5b0a68ec917bc9a5f664ff69
Astha Nayak <anshu.nayak93@gmail.com>
2022-10-18 00:00:02 +0100

    final working code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,76 +1,79 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 print(os.getcwd())
-df_degrees= pd.read_csv(r'./group-3/Resources/degrees-that-pay-back.csv')
+df_degrees= pd.read_csv(r'./Resources/degrees-that-pay-back.csv')
+print(df_degrees)
 #cleaing  the degrees data frame.
-df_degrees ['Starting Median Salary'] = df_degrees ['Starting Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees ['Starting Median Salary'] = df_degrees ['Starting Median Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees ['Starting Median Salary'] = df_degrees ['Starting Median Salary'].astype(float)
 
-df_degrees ['Mid-Career Median Salary'] = df_degrees ['Mid-Career Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees ['Mid-Career Median Salary'] = df_degrees ['Mid-Career Median Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees ['Mid-Career Median Salary'] = df_degrees ['Mid-Career Median Salary'].astype(float)
 
-df_degrees ['Mid-Career 10th Percentile Salary'] = df_degrees ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees ['Mid-Career 10th Percentile Salary'] = df_degrees ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees ['Mid-Career 10th Percentile Salary'] = df_degrees ['Mid-Career 10th Percentile Salary'].astype(float)
 
-df_degrees ['Mid-Career 25th Percentile Salary'] = df_degrees ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees ['Mid-Career 25th Percentile Salary'] = df_degrees ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees ['Mid-Career 25th Percentile Salary'] = df_degrees ['Mid-Career 25th Percentile Salary'].astype(float)
 
-df_degrees ['Mid-Career 75th Percentile Salary'] = df_degrees ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees ['Mid-Career 75th Percentile Salary'] = df_degrees ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees ['Mid-Career 75th Percentile Salary'] = df_degrees ['Mid-Career 75th Percentile Salary'].astype(float)
 
-df_degrees['Mid-Career 90th Percentile Salary'] = df_degrees['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_degrees['Mid-Career 90th Percentile Salary'] = df_degrees['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_degrees['Mid-Career 90th Percentile Salary'] = df_degrees['Mid-Career 90th Percentile Salary'].astype(float)
 
 print(df_degrees.head())
 
 #now cleaing the college data frame .
-df_college= pd.read_csv(r'./group-3/Resources/salaries-by-college-type.csv')
+df_college= pd.read_csv(r'./Resources/salaries-by-college-type.csv')
 
-df_college ['Starting Median Salary'] = df_college ['Starting Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Starting Median Salary'] = df_college ['Starting Median Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Starting Median Salary'] = df_college ['Starting Median Salary'].astype(float)
 
 
-df_college ['Mid-Career Median Salary'] = df_college ['Mid-Career Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Mid-Career Median Salary'] = df_college ['Mid-Career Median Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Mid-Career Median Salary'] = df_college ['Mid-Career Median Salary'].astype(float)
 
 
-df_college ['Mid-Career 10th Percentile Salary'] = df_college ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Mid-Career 10th Percentile Salary'] = df_college ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Mid-Career 10th Percentile Salary'] = df_college ['Mid-Career 10th Percentile Salary'].astype(float)
 
-df_college ['Mid-Career 25th Percentile Salary'] = df_college ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Mid-Career 25th Percentile Salary'] = df_college ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Mid-Career 25th Percentile Salary'] = df_college ['Mid-Career 25th Percentile Salary'].astype(float)
             
             
             
-df_college ['Mid-Career 75th Percentile Salary'] = df_college ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Mid-Career 75th Percentile Salary'] = df_college ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Mid-Career 75th Percentile Salary'] = df_college ['Mid-Career 75th Percentile Salary'].astype(float)
             
 
-df_college ['Mid-Career 90th Percentile Salary'] = df_college ['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_college ['Mid-Career 90th Percentile Salary'] = df_college ['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_college ['Mid-Career 90th Percentile Salary'] = df_college ['Mid-Career 90th Percentile Salary'].astype(float)
             
  #now cleaning the region data
  
-df_region= pd.read_csv(r'./group-3/Resources/salaries-by-region.csv') 
-df_region ['Starting Median Salary'] = df_region ['Starting Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region= pd.read_csv(r'./Resources/salaries-by-region.csv') 
+df_region ['Starting Median Salary'] = df_region ['Starting Median Salary'].str.replace(',', '').str.replace('$', '')
 df_region ['Starting Median Salary'] = df_region ['Starting Median Salary'].astype(float)
 
 
-df_region ['Mid-Career Median Salary'] = df_region ['Mid-Career Median Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region ['Mid-Career Median Salary'] = df_region ['Mid-Career Median Salary'].str.replace(',', '').str.replace('$', '')
 df_region['Mid-Career Median Salary'] = df_region ['Mid-Career Median Salary'].astype(float)
 
-df_region['Mid-Career 10th Percentile Salary'] = df_region ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region['Mid-Career 10th Percentile Salary'] = df_region ['Mid-Career 10th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_region ['Mid-Career 10th Percentile Salary'] = df_region ['Mid-Career 10th Percentile Salary'].astype(float)
 
 
 
-df_region['Mid-Career 25th Percentile Salary'] = df_region ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region['Mid-Career 25th Percentile Salary'] = df_region ['Mid-Career 25th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_region ['Mid-Career 25th Percentile Salary'] = df_region ['Mid-Career 25th Percentile Salary'].astype(float)
 
-df_region['Mid-Career 75th Percentile Salary'] = df_region ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region['Mid-Career 75th Percentile Salary'] = df_region ['Mid-Career 75th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_region ['Mid-Career 75th Percentile Salary'] = df_region ['Mid-Career 75th Percentile Salary'].astype(float)
   
-df_region ['Mid-Career 90th Percentile Salary'] = df_region['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('.', '').str.replace('$', '')
+df_region ['Mid-Career 90th Percentile Salary'] = df_region['Mid-Career 90th Percentile Salary'].str.replace(',', '').str.replace('$', '')
 df_region ['Mid-Career 90th Percentile Salary'] = df_region ['Mid-Career 90th Percentile Salary'].astype(float)
                                                             
                                                                          
